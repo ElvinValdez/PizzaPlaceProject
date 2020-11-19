cde87058c18d137c5ffa6816a6b15c5e48264125
JadaPerryman <jadaperryman@mail.adelphi.edu>
2020-11-19 16:48:49 -0400

    Routes and drink views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function() {
+    return view('admin.dashboard');
+});
+
+Route::get('/manage-order', function() {
+    return view('admin.manageorder');
+});
+
+Route::get('/login', function() {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function() {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/drink/create', function() {
+    return view('drink.create');
+});
+
+Route::get('/drink/edit', function() {
+    return view('drink.edit');
+});
+
