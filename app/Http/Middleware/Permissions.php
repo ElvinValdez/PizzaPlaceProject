2dc9eee558bd7d5882249b5e733f287e8edac583
Elvin Valdez <elvin.valdez@outlook.com>
2020-12-03 09:49:21 -0400

    Middleware for permissions

---
+++ app/Http/Middleware/Permissions.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Str;
+use Closure;
+
+class Permissions
+{
+    private $exceptNames = [
+    ];
+
+    private $exceptControllers = [
+        'LoginController',
+        'ForgotPasswordController',
+        'ResetPasswordController',
+        'RegisterController',
+        'ConfirmPasswordController',
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $permission = $request->route()->getName();
+
+        if ($this->match($request->route()) && auth()->user()->canNot($permission))
+            throw new UnauthorizedException(403, 'User does not have the permission: ' . $permission);
+
+        return $next($request);
+    }
+
+    private function match(Route $route)
+    {
+        if ($route->getName() == '' || $route->getName() === null) {
+            return false;
+        } else {
+            if (in_array(class_basename($route->getController()), $this->exceptControllers))
+                return false;
+            foreach ($this->exceptNames as $except)
+                if (Str::is($except, $route->getName()))
+                    return false;
+        }
+        return true;
+    }
+}
